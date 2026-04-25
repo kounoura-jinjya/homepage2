91886a2620f3f0cc78d378c612b6f1a6acd7b3b4
kounoura-jinjya <kounoura.jinjya@gmail.com>
2026-04-25 19:43:21 +0900

    script.js内を変更

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const fades = document.querySelectorAll('.fade');
 
-window.addEventListener('scroll', () => {
+function checkFades() {
   const trigger = window.innerHeight * 0.85;
-
   fades.forEach(el => {
     if (el.getBoundingClientRect().top < trigger) {
       el.classList.add('show');
     }
   });
-});
+}
+
+window.addEventListener('scroll', checkFades);
+checkFades();
